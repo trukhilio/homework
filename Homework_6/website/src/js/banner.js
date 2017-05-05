f77b929472b53e0fc60f7c09830ac80efedc40f0
trukhilio <trukhantech@gmail.com>
2017-05-05 15:02:17 +0300

    remake slider

--- Homework_6/website/src/js/banner.js
+++ Homework_6/website/src/js/banner.js
@@ -1,48 +1,64 @@
-let slideIndex = 0;
-let selectorFinder = (selector)=> document.querySelectorAll(selector);
-let selectorOne = (selector)=> document.querySelector(selector);
+const selectorFinder = (selector)=> document.querySelectorAll(selector);
+const selectorOne = (selector)=> document.querySelector(selector);
 
-let slideShow = (n, selector)=>{
-    let slide = selectorFinder('.banner__slides_item');
-    if (n > slide.length-1) {slideIndex = 0; n=slideIndex}
-    if (n < 0) {slideIndex = slide.length-1; n=slideIndex}
-    let x = 0;
-    if (selector==='banner__slides_moveOne') {x = n-1}
-    if (selector==='banner__slides_moveTwo') {x = n+1}
-    if(x<0){x=slide.length-1}
-    if(x>slide.length-1) {x=0}
-    let item = Array.from(slide, (item)=>
-        [item.classList.remove('banner__slides_on'),
-        item.classList.remove('banner__slides_off'),
-        item.classList.remove(selector)]);
+const slide = selectorFinder('.banner__slides_item');
+let arr = Array.from(slide);
 
-    slide[x].classList.add(selector);
-    slide[n].classList.add(selector);
-    slide[n].classList.add('banner__slides_off');
-    setTimeout(()=>{
-        slide[x].classList.remove(selector);
-        slide[n].classList.remove('banner__slides_off');
-        slide[n].classList.remove(selector);
-        slide[n].classList.add('banner__slides_on');
-    }, 500);
+const slideShow = (selector)=>{
+    Array.prototype.move = (old_index, new_index) =>{
+        if (new_index >= arr.length) {
+            let k = new_index - arr.length;
+            while ((k--) + 1) {
+                arr.push(undefined);
+            }
+        }
+        arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    };
+
+    let itemAdd = arr.map((item)=>item.classList.add(selector));
+
+    arr[2].classList.add('banner__slides_one');
+    arr[2].classList.remove('banner__slides_two', 'banner__slides_three');
+
+    arr[0].classList.add('banner__slides_two');
+    arr[0].classList.remove('banner__slides_one', 'banner__slides_three');
+
+    arr[1].classList.add('banner__slides_three');
+    arr[1].classList.remove('banner__slides_one', 'banner__slides_two');
+
+    let inTimerFunc = (firstItem, lastItem)=>{
+        arr.move(firstItem, lastItem);
+        arr[0].classList.add('banner__slides_one');
+        arr[0].classList.remove('banner__slides_two', 'banner__slides_three');
+
+        arr[1].classList.add('banner__slides_two');
+        arr[1].classList.remove('banner__slides_one', 'banner__slides_three');
+
+        arr[2].classList.add('banner__slides_three');
+        arr[2].classList.remove('banner__slides_one', 'banner__slides_two');
+
+        let itemRemove = arr.map((item)=>item.classList.remove(selector));
+    };
+
+    if (selector==='banner__slides_moveOne'){
+        setTimeout(()=>{inTimerFunc(0, arr.length-1)}, 1000);
+    }
+
+    if (selector==='banner__slides_moveTwo')
+        setTimeout(()=>{inTimerFunc(arr.length-1, 0)}, 1000);
 };
 
-let plusOne =(n, selector)=>{
-    slideIndex +=n;
-    slideShow(slideIndex, selector);
-};
-
-let slideTime = setInterval(()=>plusOne(1,'banner__slides_moveOne'), 5000);
+let slideTime = setInterval(()=>slideShow('banner__slides_moveOne'), 5000);
 
 let selectLeft = selectorOne('.arrow__item_left');
 let selectRight = selectorOne('.arrow__item_right');
 
-let slideNext = (n, selector)=>{
+let slideNext = (selector)=>{
     clearInterval(slideTime);
-    plusOne(n, selector);
-    slideTime = setInterval(()=>plusOne(1,'banner__slides_moveOne'), 5000);
+    slideShow(selector);
+    slideTime = setInterval(()=>slideShow('banner__slides_moveOne'), 5000);
 };
 
-selectLeft.addEventListener('click', ()=>slideNext(-1, 'banner__slides_moveTwo'), false);
-selectRight.addEventListener('click', ()=>slideNext(1, 'banner__slides_moveOne'), false);
+selectLeft.addEventListener('click', ()=>slideNext('banner__slides_moveTwo'), false);
+selectRight.addEventListener('click', ()=>slideNext('banner__slides_moveOne'), false);
 
